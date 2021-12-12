f3172a12c07a3b2a1aabcc5fda30a51bdef15e28
rauf <nraufu@gmail.com>
2021-12-12 22:42:23 +0200

    initial setup :tada:

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <the-header></the-header>
+
+    <div class="quotes-container">
+      <quote></quote>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/layout/TheHeader.vue";
+import Quote from "./components/quote/Quote.vue";
+
+export default {
+  name: "App",
+  components: {
+    TheHeader,
+    Quote,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&display=swap");
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  font-family: "Raleway", sans-serif;
+  color: #000;
+  background: #fff;
+}
+</style>
---
+++ src/components/quote/Quote.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="quote">quote</div>
+</template>
